ef01175a2c989f8cda29148391bdd4586b0ad44c
Vsollery <vsollery@gmail.com>
2023-03-09 13:31:23 +0700

    add command

--- MVVM/ViewModels/DataViewModel.cs
+++ MVVM/ViewModels/DataViewModel.cs
@@ -30,6 +30,8 @@
 
                 new DataViewModel(typeof(HslColorPage),"Color", "Use a view model to select HSL colors"),
 
+                 new DataViewModel(typeof(KeypadPage),"Keypad", "Use a view model for numeric keypad logic"),
+
             };
         }
         public static IList<DataViewModel> All { private set; get; }
---
+++ MVVM/Views/Information.xaml.cs
@@ -0,0 +1,9 @@
+namespace MVVM.Views;
+
+public partial class Information : ContentPage
+{
+	public Information()
+	{
+		InitializeComponent();
+	}
+}
