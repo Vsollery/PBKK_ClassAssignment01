ec8b747468e15861d2c5f760bcf6efd7ee306c87
Vsollery <vsollery@gmail.com>
2023-03-09 11:51:45 +0700

    simplee binding

---
+++ MVVM/ViewModels/DataViewModel.cs
@@ -0,0 +1,34 @@
+﻿namespace MVVM
+{
+    public class DataViewModel
+    {
+        public DataViewModel(Type type, string title, string description)
+        {
+            Type = type;
+            Title = title;
+            Description = description;
+        }
+
+        public Type Type { private set; get; }
+
+        public string Title { private set; get; }
+
+        public string Description { private set; get; }
+
+        static DataViewModel()
+        {
+            All = new List<DataViewModel>
+            {
+  
+
+                // Data Bindings and MVVM
+
+                //Get current date and time.
+                new DataViewModel(typeof(DateTimePage), "DateTime", "Obtain the current DateTime and display it"),
+
+            };
+        }
+        public static IList<DataViewModel> All { private set; get; }
+
+    }
+}
